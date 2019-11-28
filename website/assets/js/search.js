99401ddd1554fd88c0a35c6420ce7aa01a3c027a
sahbikhlifi <khlifisahbi@gmail.com>
2019-11-28 16:32:10 +0100

    search

--- website/assets/js/search.js
+++ website/assets/js/search.js
@@ -1,6 +1,22 @@
-function search() {
-    var listHouse = JSON.parse(localStorage.getItem("task"))
+// function searchAnn() {
+
+//     var citeapp = document.getElementById("cite").value;
+//     var rent1 = document.getElementById("rent").value;
+//     var type = document.getElementById("type").value;
+//     var nbBedroom = document.getElementById("bedrooms").value;
+//     var nbBath = document.getElementById("bathrooms").value;
+//     var test = false;
+//     var listAnnonce = JSON.parse(localStorage.getItem("annonce"));
 
 
 
-}
+//     for (i = 0; i < listAnnonce.length; i++) {}
+
+//     if (citeapp == listAnnonce[i].Adress && rent1 == listAnnonce[i].rent && type == listAnnonce[i].type && nbBedroom == listAnnonce[i].nbbedrooms && nbBath == listAnnonce[i].nbbathrooms)
+
+//     {
+//         localStorage.setItem('listSearch', JSON.stringify(listAnnonce[i]));
+//         window.location = "/properties-left-sidebar.html";
+//         test = true;
+//     }
+// }
--- website/crudannonce.js
+++ website/crudannonce.js
@@ -1,7 +1,7 @@
 function addAnnonce() {
 
     // book a space on localstorage as a list
-    let listAnnonce = JSON.parse(localStorage.getItem("Annonce"));
+    let listAnnonce = JSON.parse(localStorage.getItem("annonce"));
 
     if (listAnnonce == null) {
         listAnnonce = [];
@@ -11,6 +11,7 @@ function addAnnonce() {
 
     // structure of object to add in localstorage
     var Annonce = {
+        id: Math.floor(Math.random() * 1000 + 1),
         title: document.getElementById("property_title").value,
         Adress: document.getElementById("property_address").value,
         rent: document.getElementById("rentinput").value,
@@ -32,8 +33,43 @@ function addAnnonce() {
     // push the structure and setitem as a string
     listAnnonce.push(Annonce);
     localStorage.setItem("annonce", JSON.stringify(listAnnonce));
-    console.log(localStorage.getItem("Annonce"));
+    console.log(localStorage.getItem("annonce"));
     // } else {
     // console.log("you have to enter details ");
     // }
+}
+
+function searchAnn() {
+    var listAnnonce = JSON.parse(localStorage.getItem("annonce"));
+    var citeapp = document.getElementById("cite").value;
+    var rent1 = document.getElementById("rent").value;
+    var type = document.getElementById("type").value;
+    var nbBedroom = document.getElementById("bedrooms").value;
+    var nbBath = document.getElementById("bathrooms").value;
+    var test = false;
+
+    for (i = 0; i < listAnnonce.length; i++) {
+
+        if (citeapp == listAnnonce[i].Adress)
+        //  && rent1 == listAnnonce[i].rent && type == listAnnonce[i].type && nbBedroom == listAnnonce[i].nbbedrooms && nbBath == listAnnonce[i].nbbathrooms)
+
+        {
+            localStorage.setItem('listSearch', JSON.stringify(listAnnonce[i]));
+            window.location = "website/properties-left-sidebar";
+            test = true;
+            break;
+
+
+        }
+
+
+    }
+}
+
+function displaySearch() {
+    var listAnnonce = JSON.parse(localStorage.getItem("annonce"));
+    var listSearch = JSON.parse(localStorage.getItem("listSearch"))
+
+
+
 }
