5e1933b1701d46f3983ae9c28e7ebeb24367b908
naim <naim.bouallagui.8@gmail.com>
2019-12-05 16:03:03 +0100

    display list Clients

--- AdminKerya/index.js
+++ AdminKerya/index.js
@@ -1,3 +1,43 @@
+
+function listClients() {
+  let clients = JSON.parse(localStorage.getItem("userList")) || [];
+  let html = "";
+  var j=0
+       
+  html = ` <table id="clients_table">
+<thead>
+  <tr>
+      <th style="width: 1%">id</th>
+      <th style="width: 20%">Username</th>
+      <th style="width: 30%">Address</th>
+      <th style="width: 8%">E-mail</th>
+      <th style="width: 20%">Phone</th>
+  </tr>
+  </thead>`;
+  // loop and display the items of localstorage object
+  for (let i = 0; i < clients.length; i++) {
+    j+=1;
+          html += `  <tr>
+      <td style="width: 1%">${j}</td>
+      <td style="width: 20%">${clients[i].username}</td>
+      <td style="width: 30%">${clients[i].adress}</td>
+      <td class="project-actions" style="width: 20%">
+      ${clients[i].email}
+        </td>
+      <td class="project-actions" style="width: 20%">
+      ${clients[i].number}
+      </td>
+  </tr>
+  `;
+  }
+  // add the rest tag of table
+  html += ` </table>`;
+  // inject html
+  document.getElementById("clients_table").innerHTML = html;
+}
+
+
+
 function listAnnonce() {
     let annonce = JSON.parse(localStorage.getItem("annonce")) || [];
     // let loggedUser = JSON.parse(localStorage.getItem("loggedUser"));
@@ -143,6 +183,7 @@ function deny(id) {
 
     }
     localStorage.setItem("annonce", JSON.stringify(annonceList));
+    location.reload();
 
 }
 
@@ -252,58 +293,4 @@ function listAnnonceDenied() {
     document.getElementById("annonce_table_denied").innerHTML = html;
 }
 
-function listClients() {
-  let clients = JSON.parse(localStorage.getItem("annonce")) || [];
-  // let loggedUser = JSON.parse(localStorage.getItem("loggedUser"));
-  // var contain a html table
-  let html = ` <table id="annonce_table">
-<thead>
-  <tr>
-      <th style="width: 1%">id</th>
-      <th style="width: 20%">Annonce Title</th>
-      <th style="width: 30%">Type</th>
-      <th style="width: 8%" class="text-center">Status</th>
-      <th style="width: 20%"></th>
-  </tr>
-  </thead>`;
-  // loop and display the items of localstorage object
 
-  for (let i = 0; i < annonce.length; i++) {
-      if (annonce[i].checked === "inProgress") {
-          html += `  <tr>
-      <td style="width: 1%">${annonce[i].id}</td>
-      <td style="width: 20%">${annonce[i].title}</td>
-      <td style="width: 30%">${annonce[i].type}</td>
-      <td class="project-actions text-right" style="width: 20%">
-      <a class="btn btn-success btn-sm"  onclick="activate(${annonce[i].id})">
-            <i class="fas fa-check">
-            </i>
-            Activate
-        </a>
-        <a class="btn btn-warning btn-sm"  onclick="deny(${annonce[i].id})">
-            <i class="fas fa-ban">
-            </i>
-            Deny
-        </a>
-        </td>
-      <td class="project-actions text-right" style="width: 20%">
-        <a class="btn btn-primary btn-sm" href="#" data-toggle="modal" data-target="#modal-default" onclick="modalDisplay(${annonce[i].id})">
-            <i class="fas fa-folder">
-            </i>
-            View
-        </a>
-        <a class="btn btn-danger btn-sm" data-toggle="modal" data-target="#modal-default" onclick="displayDelete(${annonce[i].id},'${annonce[i].title}')">
-            <i class="fas fa-trash">
-            </i>
-            Delete
-        </a>
-      </td>
-  </tr>
-  `;
-      }
-  }
-  // add the rest tag of table
-  html += ` </table>`;
-  // inject html
-  document.getElementById("annonce_table").innerHTML = html;
-}
